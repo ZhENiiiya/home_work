d704c8c6b892f697ddd85b4b070b56a29fef6b8f
ZhENiiiya <zhenyaeliseev8@yandex.ru>
2022-12-23 22:28:54 +0300

    add method

--- home_work_6_Sem2/Program.cs
+++ home_work_6_Sem2/Program.cs
@@ -4,27 +4,31 @@
 //32679 -> 6
 
 
+void NumberDiv(int num)
+{
+    if (num > 99)
+    {
+        int counter = 0;
+        for (int i = num; i != 0; i = i / 10)
+        {
+            counter++;
+        }
+
+        int[] numberSet = new int[counter];
+
+        for (int x = num; x != 0; x = x / 10)
+        {
+            numberSet[counter - 1] = x;
+            counter--;
+        }
+        Console.WriteLine(num + " -> " + (numberSet[2] % 10));
+    }
+    else
+    {
+        Console.WriteLine(num + " -> третьей цифры нет");
+    }
+}
+
 Console.Write("Введите число - ");
 int num = int.Parse(Console.ReadLine());
-
-if (num > 99)
-{
-    int counter = 0;
-    for (int i = num; i != 0; i = i / 10)
-    {
-        counter++;
-    }
-
-    int[] numberSet = new int [counter];
-
-    for (int x = num; x != 0; x = x / 10)
-    {
-        numberSet[counter - 1] = x;
-        counter--;
-    }
-    Console.WriteLine(num + " -> " + (numberSet[2] % 10));
-}
-else
-{
-    Console.WriteLine(num + " -> третьей цифры нет");
-}
+NumberDiv(num);
