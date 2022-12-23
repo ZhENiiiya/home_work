7e504f8ac10f162db2cb76c73e93ae814f029cfe
ZhENiiiya <zhenyaeliseev8@yandex.ru>
2022-12-23 22:03:50 +0300

    update new code home_work_6_Sem2

--- home_work_6_Sem2/Program.cs
+++ home_work_6_Sem2/Program.cs
@@ -9,13 +9,22 @@ int num = int.Parse(Console.ReadLine());
 
 if (num > 99)
 {
+    int counter = 0;
     for (int i = num; i != 0; i = i / 10)
     {
-        Console.WriteLine(i);
+        counter++;
     }
 
+    int[] numberSet = new int [counter];
+
+    for (int x = num; x != 0; x = x / 10)
+    {
+        numberSet[counter - 1] = x;
+        counter--;
+    }
+    Console.WriteLine(num + " -> " + (numberSet[2] % 10));
 }
 else
 {
-    Console.WriteLine(num + " третьей цифры нет");
+    Console.WriteLine(num + " -> третьей цифры нет");
 }
