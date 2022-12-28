ae4f5f1be14234027e030ae2f36b8739a42bd5a4
Eugen Eugen <zhenya@localhost>
2022-12-28 14:30:47 +0300

    add home_work_10_Sem3

---
+++ home_work_10_Sem3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int ReadNumber(string s)
+
+{
+    while (true)
+    {
+        Console.Write(s);
+        string text = Console.ReadLine();
+        if (int.TryParse(text, out int number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Невверный ввод");
+        }
+    }
+}
+
+int num = ReadNumber("Введите число - ");
+Console.Write(num + " -> ");
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write(i*i*i + " ");
+}
