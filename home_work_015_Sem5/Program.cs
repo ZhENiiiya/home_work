0f2d5c5decd633c520545038bd48d09238c7d25e
Eugen Eugen <zhenya@localhost>
2023-01-03 15:50:29 +0300

    upload home_work_015_Sem5

---
+++ home_work_015_Sem5/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = CreateRandomArray(4, -99, 99);
+Console.WriteLine($"{PrintArray(array)} -> {GetSumUnevenIndex(array)}");
+
+
+int GetSumUnevenIndex(int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        result += array[i];
+    }
+    return result;
+}
+
+
+
+string PrintArray(int[] array)
+{
+    return ($"[{string.Join(", ", array)}]");
+}
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    Random random = new Random();
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
