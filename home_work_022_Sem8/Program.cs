8534864605202a7b09a523fd7cd7eb50db651e79
Eugen Eugen <zhenya@localhost>
2023-01-11 22:33:27 +0300

    upload home_work_022_Sem8

---
+++ home_work_022_Sem8/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] array2D = new int[,]{ {1, 4, 7, 2},
+                            {5, 9, 2, 3},
+                            {8, 4, 2, 4} };
+Print2DArray(array2D);
+Console.WriteLine();
+Print2DArray(SortArray2D(array2D));
+
+int[,] SortArray2D(int[,] array2D)
+{
+    
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array2D.GetLength(1)-1; k++)
+            {
+                if (array2D[i, k] < array2D[i, k+1])
+                {
+                    int x = array2D[i, k+1];
+                    array2D[i,k+1] = array2D[i,k];
+                    array2D[i,k] = x;
+                }
+            }
+        }
+    }
+
+    return array2D;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
