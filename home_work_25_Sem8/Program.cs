3e39a77a9e602fec9bf5e749b08b6f8c9aeb49f6
Eugen Eugen <zhenya@localhost>
2023-01-13 20:11:30 +0300

    Uploads home_work_25_Sem8

---
+++ home_work_25_Sem8/Program.cs
@@ -0,0 +1,46 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся 
+// двузначных чисел. 
+// Напишите программу, которая будет построчно 
+// выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+Print3DArray(CreateArray3D(2, 2, 2, 1, 99));
+
+int[,,] CreateArray3D(int m, int n, int x, int randomX, int randomY)
+{
+    Random ran = new Random();
+    int[,,] array3D = new int[m, n, x];
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                int xm = ran.Next(randomX, randomY);
+                array3D[i, j, k] =  ran.Next(randomX, randomY);
+            }
+        }
+    }
+
+    return array3D;
+}
+
+void Print3DArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
