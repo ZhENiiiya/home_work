acd5dca4f5fc14e7905532584c9b220727fb8e54
Eugen Eugen <zhenya@localhost>
2023-01-15 16:46:41 +0300

    upload home_work_027_Sem9

--- home_work_027_Sem9/Program.cs
+++ home_work_027_Sem9/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. 
+// Напишите программу, 
+// которая выведет все натуральные числа в 
+// промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int n = 8;
+Console.Write(n + " -> ");
+PrintNum(n);
+
+
+void PrintNum(int n)
+{
+    if (n == 0);
+    else 
+    {
+        Console.Write(n + " ");
+        PrintNum(n-1);
+    }
+}
