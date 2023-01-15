ced4a8e74a180ab81e1fcc96b03e4d06dd5df9e1
Eugen Eugen <zhenya@localhost>
2023-01-15 16:54:46 +0300

    upload home_work_028_Sem9

---
+++ home_work_028_Sem9/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int m = 4;
+int n = 8;
+
+Console.WriteLine(SumNum(m, n));
+
+int SumNum(int m, int n)
+{
+    int res = 0;
+    if(n >= m )
+    {
+        res += m;
+        m++;
+    }
+    else
+    {
+        return res;
+    }
+    return res + SumNum(m, n);
+}
